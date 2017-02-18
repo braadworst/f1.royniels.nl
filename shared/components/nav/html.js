e84cab111bf88a7b0560d00df92a12a8dcefc3de
braadworst <roy@royniels.nl>
2017-02-18 09:42:45 +0800

    fixed navigatition

--- shared/components/nav/html.js
+++ shared/components/nav/html.js
@@ -3,18 +3,23 @@ let url = require('../../helpers/url');
 module.exports = function(step) {
   return `
   <component-nav>
-    <ul>
-      <li><a href="${ url.home() }">Home</a></li>
-      <li><a href="${ url.login() }">login</a></li>
-      <li><a href="${ url.logout() }">logout</a></li>
-      <li><a href="${ url.register() }">register</a></li>
-      <li><a href="${ url.teams() }">teams</a></li>
-      <li><a href="${ url.teamDetail('bla') }">team detail</a></li>
-      <li><a href="${ url.teamCreate() }">Team create</a></li>
-      <li><a href="${ url.races() }">Races</a></li>
-      <li><a href="${ url.standings() }">Standings</a></li>
-      <li><a href="${ url.rules() }">Rules</a></li>
-    </ul>
+    <nav>
+      <a href="" class="close"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i></a>
+      <ul>
+        <li>
+          <div class="brand">
+            <span class="logo first"></span><span class="logo second"></span><span class="logo third"></span><span class="logo fourth"></span>
+            <span class="name">F1 Manager</span>
+          </div>
+        </li>
+        <li><a href="${ url.teamCreate() }">Create team</a></li>
+        <li><a href="${ url.teams() }">All teams</a></li>
+        <li><a href="${ url.races() }">Races</a></li>
+        <li><a href="${ url.standings() }">Standings</a></li>
+        <li><a href="${ url.rules() }">Rules</a></li>
+        <li><a href="${ url.logout() }">Logout</a></li>
+      </ul>
+    </nav>
   </component-nav>
   `;
 }
--- shared/components/nav/index.js
+++ shared/components/nav/index.js
@@ -1,9 +1,49 @@
 const html  = require('./html');
+const watch = require('redux-watch');
 
 module.exports = function() {
+
+  let unsubscribe;
+
   return {
     create(renderer, store) {
       renderer.render(html());
+    },
+    init(renderer, store) {
+      subscribe(renderer, store);
+      setActive(store.getState().menuActive.active);
+      switcher();
+    },
+    added(renderer, store) {
+      subscribe(renderer, store);
+    },
+    removed() {
+      unsubscribe();
     }
+  }
+
+  function switcher() {
+    const switcher = document.querySelector('component-nav .close');
+    switcher.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector('component-nav').classList.toggle('closed');
+      document.querySelector('main').classList.toggle('full');
+    });
+  }
+
+  function setActive(active) {
+    const links = [].slice.call(document.querySelectorAll('component-nav a'));
+    links.forEach(link => {
+      if (link.getAttribute('href') === active) {
+        link.classList.add('active');
+      } else if (link.classList.contains('active')) {
+        link.classList.remove('active');
+      }
+    });
+  }
+
+  function subscribe(renderer, store) {
+    let watcher = watch(store.getState, 'menuActive.active');
+    unsubscribe = store.subscribe(watcher(active => setActive(active)));
   }
 }
