10f9ee4f2a727f634a94478df9e1d9b0c6f55221
braadworst <roy@royniels.nl>
2017-02-18 13:54:21 +0800

    added standings

--- shared/components/standings/html.js
+++ shared/components/standings/html.js
@@ -1,7 +1,261 @@
 module.exports = function(options = {}) {
   return `
-  <component-home>
-    <h1>Home</h1>
-  </component-home>
+  <component-standings>
+    <h1>Standings</h1>
+    <ul class="group-standings">
+    <li>
+      <div class="item-standings item-standings-first">
+        <div class="item-header">
+          1
+        </div>
+        <div class="item-body">
+          <h1>Schumacher is in coma</h1>
+          <p>Roy Niels</p>
+        </div>
+        <div class="item-footer">
+          237
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          2
+        </div>
+        <div class="item-body">
+          <h1>C# for life</h1>
+          <p>Alex Ubbink</p>
+        </div>
+        <div class="item-footer">
+          221
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          3
+        </div>
+        <div class="item-body">
+          <h1>Ubuntu werkt prima!</h1>
+          <p>Marc Niels</p>
+        </div>
+        <div class="item-footer">
+          189
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings item-standings-first">
+        <div class="item-header">
+          1
+        </div>
+        <div class="item-body">
+          <h1>Schumacher is in coma</h1>
+          <p>Roy Niels</p>
+        </div>
+        <div class="item-footer">
+          237
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          2
+        </div>
+        <div class="item-body">
+          <h1>C# for life</h1>
+          <p>Alex Ubbink</p>
+        </div>
+        <div class="item-footer">
+          221
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          3
+        </div>
+        <div class="item-body">
+          <h1>Ubuntu werkt prima!</h1>
+          <p>Marc Niels</p>
+        </div>
+        <div class="item-footer">
+          189
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings item-standings-first">
+        <div class="item-header">
+          1
+        </div>
+        <div class="item-body">
+          <h1>Schumacher is in coma</h1>
+          <p>Roy Niels</p>
+        </div>
+        <div class="item-footer">
+          237
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          2
+        </div>
+        <div class="item-body">
+          <h1>C# for life</h1>
+          <p>Alex Ubbink</p>
+        </div>
+        <div class="item-footer">
+          221
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          3
+        </div>
+        <div class="item-body">
+          <h1>Ubuntu werkt prima!</h1>
+          <p>Marc Niels</p>
+        </div>
+        <div class="item-footer">
+          189
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings item-standings-first">
+        <div class="item-header">
+          1
+        </div>
+        <div class="item-body">
+          <h1>Schumacher is in coma</h1>
+          <p>Roy Niels</p>
+        </div>
+        <div class="item-footer">
+          237
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          2
+        </div>
+        <div class="item-body">
+          <h1>C# for life</h1>
+          <p>Alex Ubbink</p>
+        </div>
+        <div class="item-footer">
+          221
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          3
+        </div>
+        <div class="item-body">
+          <h1>Ubuntu werkt prima!</h1>
+          <p>Marc Niels</p>
+        </div>
+        <div class="item-footer">
+          189
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings item-standings-first">
+        <div class="item-header">
+          1
+        </div>
+        <div class="item-body">
+          <h1>Schumacher is in coma</h1>
+          <p>Roy Niels</p>
+        </div>
+        <div class="item-footer">
+          237
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          2
+        </div>
+        <div class="item-body">
+          <h1>C# for life</h1>
+          <p>Alex Ubbink</p>
+        </div>
+        <div class="item-footer">
+          221
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          3
+        </div>
+        <div class="item-body">
+          <h1>Ubuntu werkt prima!</h1>
+          <p>Marc Niels</p>
+        </div>
+        <div class="item-footer">
+          189
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings item-standings-first">
+        <div class="item-header">
+          1
+        </div>
+        <div class="item-body">
+          <h1>Schumacher is in coma</h1>
+          <p>Roy Niels</p>
+        </div>
+        <div class="item-footer">
+          237
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          2
+        </div>
+        <div class="item-body">
+          <h1>C# for life</h1>
+          <p>Alex Ubbink</p>
+        </div>
+        <div class="item-footer">
+          221
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item-standings">
+        <div class="item-header">
+          3
+        </div>
+        <div class="item-body">
+          <h1>Ubuntu werkt prima!</h1>
+          <p>Marc Niels</p>
+        </div>
+        <div class="item-footer">
+          189
+        </div>
+      </div>
+    </li>
+    </ul>
+  </component-standings>
   `;
 }
