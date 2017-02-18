2624faefbc2d9579b0c62315a8a26cb34af121a6
braadworst <roy@royniels.nl>
2017-02-18 15:58:13 +0800

    Added the races

--- shared/components/races/index.js
+++ shared/components/races/index.js
@@ -1,10 +1,31 @@
-const html = require('./html');
+const html     = require('./html');
+let   circuits = require('../../fixtures/circuits');
+const drivers  = require('../../fixtures/drivers');
+const moment   = require('moment');
 
 module.exports = function() {
 
   return {
     create(renderer) {
-      renderer.render(html());
+
+      const now = moment();
+
+      // set parameters based on the current date
+      circuits = circuits.map(circuit => {
+        return Object.assign(
+          {},
+          circuit,
+          { passed : circuit.date.isSameOrBefore(now) }
+        )
+      });
+
+      // Set upcoming race
+      circuits[circuits.findIndex(circuit => !circuit.passed)].upcoming = true;
+
+      renderer.render(html({
+        circuits,
+        drivers
+      }));
     }
   }
 }
--- shared/fixtures/circuits.js
+++ shared/fixtures/circuits.js
@@ -1,13 +1,83 @@
+const moment = require('moment');
+
 module.exports = [{
   image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Australia/_jcr_content/par/manual_gallery_0/image1.img.2048.medium.jpg/1484840593394.jpg',
   name  : 'Melbourne Grand Prix Circuit',
-  date  : '26-03-2017'
+  date  : moment('26-03-2017', 'DD-MM-YYYY')
 }, {
   image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/China/_jcr_content/par/manual_gallery_0/image1.img.2048.medium.jpg/1484587066926.jpg',
   name  : 'Shanghai International Circuit',
-  date  : '09-04-2017'
+  date  : moment('09-04-2017', 'DD-MM-YYYY')
 }, {
   image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Bahrain/_jcr_content/par/manual_gallery_0/image1.img.2048.medium.jpg/1483702811745.jpg',
   name  : 'Bahrain International Circuit',
-  date  : '16-04-2017'
+  date  : moment('16-04-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Russia/_jcr_content/par/manual_gallery_0/image2.img.2048.medium.jpg/1486484958695.jpg',
+  name  : 'Sochi Autodrom',
+  date  : moment('30-04-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Spain/_jcr_content/par/manual_gallery_0/image4.img.2048.medium.jpg/1486485010692.jpg',
+  name  : 'Circuit de Barcelona-Catalunya',
+  date  : moment('14-05-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Monaco/_jcr_content/par/manual_gallery_1/image2.img.2048.medium.jpg/1486485075251.jpg',
+  name  : 'Circuit de Monaco',
+  date  : moment('28-05-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Canada/_jcr_content/par/manual_gallery_0/image1.img.2048.medium.jpg/1486485136734.jpg',
+  name  : 'Circuit Gilles Villeneuve',
+  date  : moment('11-06-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Azerbaijan/_jcr_content/par/manual_gallery_0/image1.img.2048.medium.jpg/1486485183884.jpg',
+  name  : 'Baku City Circuit',
+  date  : moment('25-06-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Austria/_jcr_content/par/manual_gallery_0/image1.img.2048.medium.jpg/1486485320071.jpg',
+  name  : 'Red Bull Ring',
+  date  : moment('09-07-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Great_Britain/_jcr_content/par/manual_gallery_0/image1.img.2048.medium.jpg/1486485483574.jpg',
+  name  : 'Silverstone Circuit',
+  date  : moment('16-07-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Hungary/_jcr_content/par/manual_gallery_0/image2.img.2048.medium.jpg/1486485532062.jpg',
+  name  : 'Hungaroring',
+  date  : moment('30-07-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Belgium/_jcr_content/par/manual_gallery_0/image1.img.2048.medium.jpg/1486485619774.jpg',
+  name  : 'Circuit de Spa-Francorchamps',
+  date  : moment('27-08-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Italy/_jcr_content/par/manual_gallery_0/image4.img.2048.medium.jpg/1486485680749.jpg',
+  name  : 'Autodromo Nazionale Monza',
+  date  : moment('03-09-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Singapore/_jcr_content/par/manual_gallery_0/image3.img.2048.medium.jpg/1486485725925.jpg',
+  name  : 'Marina Bay Street Circuit',
+  date  : moment('17-09-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Malaysia/_jcr_content/par/manual_gallery_0/image1.img.2048.medium.jpg/1486485765667.jpg',
+  name  : 'Sepang International Circuit',
+  date  : moment('01-10-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Japan/_jcr_content/par/manual_gallery_0/image3.img.2048.medium.jpg/1486485806563.jpg',
+  name  : 'Suzuka International Racing Course',
+  date  : moment('08-10-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/United_States/_jcr_content/par/manual_gallery_0/image3.img.2048.medium.jpg/1486485846711.jpg',
+  name  : 'Circuit of the Americas',
+  date  : moment('22-10-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Mexico/_jcr_content/par/manual_gallery_0/image3.img.2048.medium.jpg/1486485895850.jpg',
+  name  : 'Autódromo Hermanos Rodríguez',
+  date  : moment('29-10-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Brazil/_jcr_content/par/manual_gallery_0/image4.img.2048.medium.jpg/1486485938857.jpg',
+  name  : 'Autódromo José Carlos Pace',
+  date  : moment('12-11-2017', 'DD-MM-YYYY')
+}, {
+  image : 'https://www.formula1.com/content/fom-website/en/championship/races/2017/Abu_Dhabi/_jcr_content/par/manual_gallery_0/image4.img.2048.medium.jpg/1486485982341.jpg',
+  name  : 'Yas Marina Circuit',
+  date  : moment('26-11-2017', 'DD-MM-YYYY')
 }]
