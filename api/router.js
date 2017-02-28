7f2d6d3b4330ec4d8b033c8f7b56f8fd740365c3
braadworst <royniels83@gmail.com>
2017-03-01 07:00:01 +0800

    Moved headers to before call

--- api/router.js
+++ api/router.js
@@ -3,31 +3,28 @@ const tables = require('./store/tables');
 module.exports = function(server, database) {
   const router = require('cs-router')(server);
 
-  router.get('/api/drivers', async function(request, response) {
-    const records = await database.selectAll(tables.drivers);
+  router.before((request, response) => {
     response.setHeader('Content-Type', 'application/vnd.api+json');
     response.setHeader('Access-Control-Allow-Origin', '*');
+  });
+
+  router.get('/api/drivers', async function(request, response) {
+    const records = await database.selectAll(tables.drivers);
     response.end(JSON.stringify(records));
   });
 
   router.get('/api/chassis', async function(request, response) {
     const records = await database.selectAll(tables.chassis);
-    response.setHeader('Content-Type', 'application/vnd.api+json');
-    response.setHeader('Access-Control-Allow-Origin', '*');
     response.end(JSON.stringify(records));
   });
 
   router.get('/api/engines', async function(request, response) {
     const records = await database.selectAll(tables.engines);
-    response.setHeader('Content-Type', 'application/vnd.api+json');
-    response.setHeader('Access-Control-Allow-Origin', '*');
     response.end(JSON.stringify(records));
   });
 
   router.get('/api/circuits', async function(request, response) {
     const records = await database.selectAll(tables.circuits);
-    response.setHeader('Content-Type', 'application/vnd.api+json');
-    response.setHeader('Access-Control-Allow-Origin', '*');
     response.end(JSON.stringify(records));
   });
 }
