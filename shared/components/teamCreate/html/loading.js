c486de4665ede708b48e98c94a099621ee2e9bdb
braadworst <royniels83@gmail.com>
2017-02-28 12:10:51 +0800

    added loading template create team

--- shared/components/teamCreate/html/loading.js
+++ shared/components/teamCreate/html/loading.js
@@ -1,21 +1,23 @@
+const range = require('lodash/range');
+
 module.exports = function() {
   return `
-  <component-team-create class="animation-page-in">
+  <component-team-create class="animation-page-in loading">
     <form class="pure-form">
       <div class="pure-g">
       <div class="pure-u-1-1 pure-u-md-1-3">
         <div class="content">
-          <h2>€ <span class="budget"><span class="filler"></span></span></h2>
+          &nbsp;
         </div>
       </div>
         <div class="pure-u-1-1 pure-u-md-1-3">
           <div class="content">
-            <input type="text" name="name" placeholder="Enter your team name..." autocomplete="off" maxlength="40">
+            &nbsp;
           </div>
         </div>
         <div class="pure-u-1-1 pure-u-md-1-3">
           <div class="content">
-            <a href="" class="button">Create team</a>
+            &nbsp;
           </div>
         </div>
       </div>
@@ -23,15 +25,15 @@ module.exports = function() {
 
     <h1 class="section-header">Select drivers</h1>
     <section class="pure-g">
-      ${ [0,0,0,0,0,0,0,0].map(rowDriver).join('') }
+      ${ range(20).map(rowDriver).join('') }
     </section>
     <h1 class="section-header">Select engines</h1>
     <section class="pure-g">
-      ${ [0,0,0].map(rowEngine).join('') }
+      ${ range(4).map(rowEngine).join('') }
     </section>
     <h1 class="section-header">Select chassis</h1>
     <section class="pure-g">
-      ${ [0,0,0].map(rowChassis).join('') }
+      ${ range(10).map(rowChassis).join('') }
     </section>
 
   </component-team-create>
@@ -43,12 +45,11 @@ function rowDriver() {
   <div class="pure-u-1-1 pure-u-md-1-3 pure-u-lg-1-4">
     <section class="item-create item-create-driver" data-price="">
       <div class="item-media">
-        <img class="pure-img" src="">
       </div>
       <div class="item-body">
-        <h1><span class="filler"></span></h1>
-        <p class="team"><span class="filler"></span></p>
-        <p><span class="price">€ <span class="filler"></span></span></p>
+        <h1>&nbsp;</h1>
+        <p class="team">&nbsp;</p>
+        <p><span class="price">&nbsp;</span></p>
       </div>
     </section>
   </div>
@@ -60,11 +61,10 @@ function rowEngine() {
   <div class="pure-u-1-1 pure-u-md-1-3 pure-u-lg-1-4">
     <section class="item-create item-create-engine" data-price="">
       <div class="item-media">
-        <img class="pure-img" src="">
       </div>
       <div class="item-body">
-        <h1><span class="filler"></span></h1>
-        <p><span class="price">€ <span class="filler"></span></span></p>
+        <h1>&nbsp;</h1>
+        <p><span class="price">&nbsp;</span></p>
       </div>
     </section>
   </div>
@@ -76,11 +76,10 @@ function rowChassis() {
   <div class="pure-u-1-1 pure-u-md-1-3 pure-u-lg-1-4">
     <section class="item-create item-create-chassis" data-price="">
       <div class="item-media">
-        <img class="pure-img" src="">
       </div>
       <div class="item-body">
-        <h1><span class="filler"></span></h1>
-        <p><span class="price">€ <span class="filler"></span></span></p>
+        <h1>&nbsp;</h1>
+        <p><span class="price">&nbsp;</span></p>
       </div>
     </section>
   </div>
