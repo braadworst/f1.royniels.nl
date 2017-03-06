0dee060c8f5db32fcad07804cb933bd4a8a71d7d
braadworst <royniels83@gmail.com>
2017-03-06 14:07:21 +0800

    made router calls async

--- shared/router/index.js
+++ shared/router/index.js
@@ -4,34 +4,34 @@ module.exports = function(state, server) {
 
   const router = require('cs-router')(server);
 
-  router.get(constants.url.URL_TEAMS, (request, response, next, relay) => {
-    relay.state.dispatch('componentCreate', 'pageSwitcher', 'teams');
-    relay.state.dispatch('menuActive', request.url);
+  router.get(constants.url.URL_TEAMS, async function(request, response, next, relay) {
+    await relay.state.dispatch('componentCreate', 'pageSwitcher', 'teams');
+    await relay.state.dispatch('menuActive', request.url);
     next();
   });
-  router.get(constants.url.URL_TEAM_DETAIL, (request, response, next, relay) => {
-    relay.state.dispatch('componentCreate', 'pageSwitcher', 'teamDetail');
-    relay.state.dispatch('menuActive', request.url);
+  router.get(constants.url.URL_TEAM_DETAIL, async function(request, response, next, relay) {
+    await relay.state.dispatch('componentCreate', 'pageSwitcher', 'teamDetail');
+    await relay.state.dispatch('menuActive', request.url);
     next();
   });
-  router.get(constants.url.URL_TEAM_CREATE, (request, response, next, relay) => {
-    relay.state.dispatch('componentCreate', 'pageSwitcher', 'teamCreate');
-    relay.state.dispatch('menuActive', request.url);
+  router.get(constants.url.URL_TEAM_CREATE, async function(request, response, next, relay) {
+    await relay.state.dispatch('componentCreate', 'pageSwitcher', 'teamCreate');
+    await relay.state.dispatch('menuActive', request.url);
     next();
   });
-  router.get(constants.url.URL_RACES, (request, response, next, relay) => {
-    relay.state.dispatch('componentCreate', 'pageSwitcher', 'races');
-    relay.state.dispatch('menuActive', request.url);
+  router.get(constants.url.URL_RACES, async function(request, response, next, relay) {
+    await relay.state.dispatch('componentCreate', 'pageSwitcher', 'races');
+    await relay.state.dispatch('menuActive', request.url);
     next();
   });
-  router.get(constants.url.URL_STANDINGS, (request, response, next, relay) => {
-    relay.state.dispatch('componentCreate', 'pageSwitcher', 'standings');
-    relay.state.dispatch('menuActive', request.url);
+  router.get(constants.url.URL_STANDINGS, async function(request, response, next, relay) {
+    await relay.state.dispatch('componentCreate', 'pageSwitcher', 'standings');
+    await relay.state.dispatch('menuActive', request.url);
     next();
   });
-  router.get(constants.url.URL_RULES, (request, response, next, relay) => {
-    relay.state.dispatch('componentCreate', 'pageSwitcher', 'rules');
-    relay.state.dispatch('menuActive', request.url);
+  router.get(constants.url.URL_RULES, async function(request, response, next, relay) {
+    await relay.state.dispatch('componentCreate', 'pageSwitcher', 'rules');
+    await relay.state.dispatch('menuActive', request.url);
     next();
   });
 
