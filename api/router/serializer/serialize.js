866562e1b804ee787b082f0e0f22cd9ef1cf73de
braadworst <royniels83@gmail.com>
2017-03-09 15:22:57 +0800

    Added outline for serializer

--- api/router/serializer/serialize.js
+++ api/router/serializer/serialize.js
@@ -1,3 +1,7 @@
-module.exports = (request, response, next) => {
-  next();
+const serializer = require('../../../shared/api/serializer');
+
+module.exports = (request, response, next, relay) => {
+  const data = serializer.serialize('drivers');
+  console.log('data: ', data);
+  next({ data });
 }
--- shared/api/serializer.js
+++ shared/api/serializer.js
@@ -1,10 +1,28 @@
-module.exports = () => {
+module.exports = (function() {
+
+  function serialize(resource) {
+    return {};
+  }
+
+  function deserialize(resource) {
+    return {};
+  }
 
   const exposed = {
-    serialize(data) {
+    serialize(type, data = []) {
+      if (typeof data === 'array') {
+        data = data.map(serialize)
+      } else {
+        data = serialize(data);
+      }
+      return {
+        data
+      };
+    },
+    deserialize(type, data) {
       return data;
     }
   };
 
   return exposed;
-}
+}());
