d358c8729e6fdcb89c62b41e4833cbea1fde7d5f
braadworst <royniels83@gmail.com>
2017-03-13 20:51:18 +0800

    Started to refactor the clientside renderer

--- src/renderer/client.js
+++ src/renderer/client.js
@@ -1,57 +1,59 @@
-module.exports = function(state) {
+module.exports = function() {
 
-  const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      if (mutation.removedNodes) {
-        mutation.removedNodes.forEach(node => {
-          if (node.getAttribute('id')) {
-            state.dispatch('componentRemoved', node.getAttribute('id'));
-          }
-        })
-      }
-      if (mutation.addedNodes) {
-        mutation.addedNodes.forEach(node => {
-          if (node.getAttribute('id')) {
-            // TODO: Dirty fix to wait for the children being loaded
-            setTimeout(function() {
-              state.dispatch('componentReady', node.getAttribute('id'));
-            }, 500);
-          }
-        })
-      }
+  let placeholders = {};
+  let callbacks = {
+    ready : false
+  }
+
+  function addObserver(placeholder) {
+    const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        if (mutation.addedNodes) {
+          mutation.addedNodes.forEach(node => {
+            if (node.getAttribute('id')) {
+              if (!callbacks.ready) {
+                throw new Error('No callback added for the renderer ready callback');
+              }
+              if (typeof callbacks.ready !== 'function') {
+                throw new Error('Renderer ready callback needs to be of type function');
+              }
+              callbacks.ready(node.getAttribute('id'));
+            }
+          })
+        }
+      });
     });
-  });
 
-  // pass in the target node, as well as the observer options
-  observer.observe(document.querySelector('body'), { childList : true, subtree : true});
+    // pass in the target node, as well as the observer options
+    observer.observe(document.querySelector(placeholder), { childList : true, subtree : false });
 
-  // Run the ready call for on page elements
-  const components = [].slice.call(document.querySelectorAll(`[id]`));
-  components.forEach(component => {
-    const name = component.getAttribute('id');
-    if (name) {
-      state.dispatch('componentReady', name);
-    }
-  });
-
-  function prepare(html) {
-    // Convert the string to HTML, fucked up shit dom strings :S
-    let wrapper = document.createElement('div');
-    wrapper.innerHTML = html.trim().replace( /(^|>)\s+|\s+(?=<|$)/g, '$1');
-    return wrapper.firstChild;
+    placeholder[placeholder] = observer;
   }
 
-  // Watch for render changes
+  function removeObserver(placeholder) {
+    placeholder[placeholder].disconnect();
+    delete placeholder[placeholder];
+  }
+
   return {
-    render(html) {
-      html = prepare(html);
-      const name    = html.getAttribute('id');
-      const current = document.querySelector('#' + name);
-      if (current) {
-        state.dispatch('componentAdded', name);
-        current.innerHTML = html.innerHTML;
+    ready(callback) {
+      callbacks.ready = callback;
+    },
+    render(placeholder, html) {
+      if (!palceholders[placeholder]) {
+        addObserver(placeholder);
       }
+      if (!document.querySelector(placeholder)) {
+        throw new Error(`Trying to render, but could not find placeholder ${ placeholder }`);
+      }
+      document.querySelector(placeholder).innerHTML = html;
+    }
+    clear(placeholder) {
+      if (!document.querySelector(placeholder)) {
+        throw new Error(`Trying yo remove, but could not find placeholder ${ placeholder }`);
+      }
+      removeObserver(placeholder);
+      document.querySelector(placeholder).innerHTML = '';
     }
   }
-
 };
