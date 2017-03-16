1692d7913f00339d7925a25be084c3f1d7c11844
braadworst <royniels83@gmail.com>
2017-03-17 00:13:01 +0800

    Working on the styling

--- src/components/login/loaded.js
+++ src/components/login/loaded.js
@@ -5,7 +5,6 @@ module.exports = () => {
       <span class="logo first"></span><span class="logo second"></span><span class="logo third"></span><span class="logo fourth"></span>
       <span class="name">F1 Manager</span>
     </div>
-    <p>Login with one of the following networks</p>
     <ul>
       <li><a href="/auth/google" data-router-server><i class="fa fa-google" aria-hidden="true"></i>Google</a></li>
       <li><a href="/auth/facebook" data-router-server><i class="fa fa-facebook-official" aria-hidden="true"></i>Facebook</a></li>
--- src/components/navigation/loaded.js
+++ src/components/navigation/loaded.js
@@ -5,7 +5,7 @@ module.exports = function(user, teams) {
   return `
   <section id="navigation">
     <nav>
-      <a href="" class="close"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i></a>
+      <a href="" class="close"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
       <ul>
         <li>
           <div class="brand">
@@ -13,12 +13,15 @@ module.exports = function(user, teams) {
             <span class="name">F1 Manager</span>
           </div>
         </li>
+        <li><h4>Your teams</h4></li>
         ${ showCreate() }
         ${ myTeams() }
-        <li><a href="${ paths.get('teams') }">All teams</a></li>
+        <li>&nbsp;</li>
         <li><a href="${ paths.get('predictions') }">Race predictions</a></li>
         <li><a href="${ paths.get('standings') }">Standings</a></li>
+        <li><a href="${ paths.get('teams') }">All teams</a></li>
         <li><a href="${ paths.get('rules') }">Rules</a></li>
+        <li>&nbsp;</li>
         ${ showAdmin() }
         <li><a href="${ paths.get('logout') }" data-router-server>Logout</a></li>
       </ul>
