d4219ecc004bd37726cacf1d5869e88b5e16eefc
braadworst <royniels83@gmail.com>
2017-03-17 21:23:27 +0800

    Added content type to response

--- src/middleware/htmlResponse.js
+++ src/middleware/htmlResponse.js
@@ -1,5 +1,6 @@
 module.exports = (request, response, next, relay) => {
   relay.renderer.cache(relay.api.getCache());
   const html = relay.renderer.html();
+  response.setHeader('Content-Type', 'text/html; charset=utf-8');
   response.end(html);
 }
