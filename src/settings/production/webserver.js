722d0266c5e6e5166ee5d2405e3d01f0d056f5fd
braadworst <royniels83@gmail.com>
2017-03-17 22:06:45 +0800

    Added setttings for live

--- src/settings/production/webserver.js
+++ src/settings/production/webserver.js
@@ -15,7 +15,7 @@ module.exports = {
   google: {
     clientId     : '906777770784-755g7991ejmgib606m0g95eg658h07c2.apps.googleusercontent.com',
     clientSecret : '1NK7PxbUaJaPxU-9bz4RYWRx',
-    redirectUri  : 'https://localhost:4443/auth/google/callback',
+    redirectUri  : 'https://f1.royniels.nl/auth/google/callback',
     consent: {
       url: 'https://accounts.google.com/o/oauth2/v2/auth',
       parameters: {
@@ -34,9 +34,9 @@ module.exports = {
     }
   },
   facebook: {
-    clientId     : '754074994750154',
-    clientSecret : 'c41aec90cf93cb4825bceae46c2237f9',
-    redirectUri  : 'https://localhost:4443/auth/facebook/callback',
+    clientId     : '233727290424291',
+    clientSecret : 'ca7306eef37f058332ac0fa1d241d5ef',
+    redirectUri  : 'https://f1.royniels.nl/auth/facebook/callback',
     consent: {
       url: 'https://www.facebook.com/v2.8/dialog/oauth',
       parameters: {
@@ -55,9 +55,9 @@ module.exports = {
     }
   },
   github: {
-    clientId     : '62cf31b86a186dc08f43',
-    clientSecret : 'cf3cb495f64c137e2a4a09992e8b8fc018ab80cb',
-    redirectUri  : 'https://localhost:4443/auth/github/callback',
+    clientId     : '280bc0a3cef455ee4ea2',
+    clientSecret : '20b5538de13800d14968cdaa9636940397f108e2',
+    redirectUri  : 'https://f1.royniels.nl/auth/github/callback',
     consent: {
       url: 'https://github.com/login/oauth/authorize',
       parameters: {
