74bf3b73c73aa05354eea56bd2e4c4cbcfa2edb8
braadworst <royniels83@gmail.com>
2017-03-21 09:17:19 +0800

    Working on the stats

--- src/middleware/statistics.js
+++ src/middleware/statistics.js
@@ -4,6 +4,7 @@ module.exports = async function(request, repsonse, next, relay) {
   if (relay.user) {
     try {
       const now = parse(new Date());
+      console.log(now);
       await relay.api.set('statistics', { userId : relay.user.id, path : request.url, date : now});
     } catch (error) {
       console.log(error);
